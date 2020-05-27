588076d2bb0a985e13b3ee10dde6d27e22199402
alex-chaplygin <alex_chaplygin@mail.ru>
2020-05-27 15:11:04 +0300

    Полный класс Сообщение

--- Сообщение.cs
+++ Сообщение.cs
@@ -1,32 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Drawing;
 
-namespace СоцСеть {
-    class Сообщение {
-	Пользователь пользователь;
-	string текст;
-	
-	public Сообщение(Пользователь п)
-	{
-	    пользователь = п;
-	}
-	
-	public Пользователь ПолучитьПользователя()
-	{
-	    return пользователь;
-	}
-	
-	public void ЗадатьТекст(string т)
-	{
-	    текст = т;
-	}
-	
-	public string ПолучитьТекст()
-	{
-	    return текст;
-	}
-	
-	public void ДобавитьИзображение(Image i)
-	{
-	}
+namespace СоцСеть
+{
+    class Сообщение
+    {
+        private Пользователь User;
+        private string Text;
+        private List<Image> Images = new List<Image>();
+
+        public Сообщение(Пользователь п)
+        {
+            User = п;
+            Text = "";
+        }
+        public Сообщение(Пользователь п, string с)
+        {
+            User = п;
+            Text = с;
+        }
+        public Пользователь ПолучитьПользователя()
+        {
+            return User;
+        }
+        public void ЗадатьТекст(string text)
+        {
+            Text = text;
+        }
+        public string ПолучитьТекст()
+        {
+            return Text;
+        }
+        public void ДобавитьИзображение(Image img)
+        {
+            Images.Add(img);
+        }
+        public void УдалитьИзображение(int num)
+        {
+            Images.RemoveAt(num);
+        }
+        public List<Image> ПолучитьИзображения()
+        {
+            return Images;
+        }
     }
 }
