15ce46157bfff788d2fe5346d2a94488c31805a7
alex-chaplygin <alex_chaplygin@mail.ru>
2020-05-28 15:13:36 +0300

    Добавление класса Стена

---
+++ Стена.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СоцСеть
+{
+    class Стена
+    {
+        public List<Сообщение> сообщения;
+        public Стена ()
+            {
+            сообщения = new List<Сообщение>();
+            }
+        public void Опубликовать( Сообщение сообщение)
+        {
+            сообщения.Add(сообщение);
+        }
+        public void Удалить (int номер)
+        {
+            сообщения.RemoveAt(номер);
+        }
+        public void Редактировать(int номер, Сообщение новоеСообщение)
+        {
+            сообщения.RemoveAt(номер);
+            сообщения.Insert(номер, новоеСообщение);
+        }
+    }
+}
