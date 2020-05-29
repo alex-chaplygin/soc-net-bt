3d4fb988d78bf0cc2fa853db9893bc39bbe161e4
alex-chaplygin <alex_chaplygin@mail.ru>
2020-05-29 11:57:38 +0300

    программная документация классов

--- Сообщение.cs
+++ Сообщение.cs
@@ -10,38 +10,47 @@ namespace СоцСеть
     {
         private Пользователь пользователь;
         private string текст;
-        private List<Image> изображения = new List<Image>();
+        private List<Image> изображения;
 
         public Сообщение(Пользователь п)
         {
             пользователь = п;
             текст = "";
+	    изображения = new List<Image>();
         }
+	
         public Сообщение(Пользователь п, string с)
         {
             пользователь = п;
             текст = с;
+	    изображения = new List<Image>();
         }
+	
         public Пользователь ПолучитьПользователя()
         {
             return пользователь;
         }
+	
         public void ЗадатьТекст(string т)
         {
             текст = т;
         }
+	
         public string ПолучитьТекст()
         {
             return текст;
         }
+	
         public void ДобавитьИзображение(Image и)
         {
             изображения.Add(и);
         }
+	
         public void УдалитьИзображение(int номер)
         {
             изображения.RemoveAt(номер);
         }
+	
         public List<Image> ПолучитьИзображения()
         {
             return изображения;
