49e17bbb0a7620d11bd9fedf0611a283c507817a
alex-chaplygin <alex_chaplygin@mail.ru>
2020-06-25 16:20:08 +0300

    Прием файла на сервере

--- Сервер.cs
+++ Сервер.cs
@@ -100,6 +100,8 @@ namespace СоцСеть {
                     ответ = УдалитьПубликацию(Convert.ToInt32(команда[1]), Convert.ToInt32(команда[2]));
                 else if (команда[0] == "Удалить")
                     ответ = УдалитьПользователя(команда[1], команда[2]);
+		else if (команда[0] == "Файл")
+                    ответ = Файл(команда[1], команда[2]);
 		else
 		    ответ = "Неизвестная команда";
 	    } catch (Exception e) {
@@ -473,6 +475,17 @@ namespace СоцСеть {
                 if (k.Value.ПолучитьИмя() == имя)
                     активныеПользователи.Remove(k.Key);
             return $"Пользователь \"{имя}\" удален";
+        }
+
+	static string Файл(string fileName, string encodedFile)
+        {
+            if (!Directory.Exists("Загрузки"))
+                Directory.CreateDirectory("Загрузки");
+            fileName = $@"Загрузки\{fileName}";
+            var file = Convert.FromBase64String(encodedFile);
+            string decodedFile = Encoding.UTF8.GetString(file);
+            File.WriteAllText(fileName, decodedFile);
+            return $"Файл \"{fileName}\" загружен на сервер";
         }
     }
 }
